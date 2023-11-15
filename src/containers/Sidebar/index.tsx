88bbff84452c1828e5c8bf667b7fe6bcbdd21a13
Thalles Rafael <thallesrafaell@outlook.com>
2023-11-15 19:26:39 -0300

    CRIANDO LAYOUT DO PROJETO

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/thallesrafaell.png" />
+    <Titulo>Thalles Rafael</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, officia
+      amet. Repudiandae, officiis. Sed incidunt iste illum molestias cupiditate
+      repudiandae quia provident rerum tempora magnam! Beatae cupiditate
+      pariatur ducimus dolores.
+    </p>
+  </section>
+)
+
+export default Sobre
